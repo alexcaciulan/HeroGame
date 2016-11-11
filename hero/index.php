4ca661b8c8c908c049889154191112867d05cdd8
alexcaciulan <alexandru.caciulan@gmail.com>
2016-11-11 17:53:42 +0200

    Add files via upload

--- hero/index.php
+++ hero/index.php
@@ -1,12 +1,13 @@
 <?php
 require_once "autoload.php";
+
 require_once "includes/header.php";
 ?>
 
     <div class="jumbotron">
         <h3>The Story</h3>
         <hr>
-        <div>
+        <div style="font-size: 12px">
             <h4>Hero Orderus</h4>
             Once upon a time there was a great hero, called Orderus, with some strengths and weaknesses, as all heroes have.
             After battling all kinds of monsters for more than a hundred years, Orderus now has the following stats:
@@ -26,7 +27,7 @@ require_once "includes/header.php";
             </ul>
         </div>
         <hr>
-        <div>
+        <div style="font-size: 12px">
             <h4>The beast</h4>
             As Orderus walks the ever-green forests of Emagia, he encounters some wild beasts, with the following properties:
             <ul>
@@ -36,6 +37,31 @@ require_once "includes/header.php";
                 <li>Speed: 40 – 60</li>
                 <li>Luck: 25% - 40%</li>
             </ul>
+        </div>
+        <hr>
+        <div style="font-size: 12px">
+            <h4>Gameplay Rules</h4>
+            You’ll have to simulate a battle between Orderus and a wild beast, either at command line or using a web browser. On every battle, Orderus and the beast must be initialized with random properties, within their ranges.<br />
+            *The first attack is done by the player with the higher speed. If both players have the same speed,
+            than the attack is carried on by the player with the highest luck.<br />
+
+            *After an attack, the players switch roles: the attacker now defends and the defender now attacks.<br /><br />
+
+            The damage done by the attacker is calculated with the following formula:<br />
+
+            <strong>Damage = Attacker strength – Defender defence</strong><br />
+
+            -The damage is subtracted from the defender’s health.<br />
+            -An attacker can miss their hit and do no damage if the defender gets lucky that turn.<br />
+            -Orderus’ skills occur randomly, based on their chances, so take them into account on each turn.
+
+            <h4>Game over</h4>
+
+            The game ends when one of the players remain without health or the number of turns reaches 20.
+            The application must output the results each turn: what happened, which skills were used (if any),
+            the damage done, defender’s health left.
+            If we have a winner before the maximum number of rounds is reached, he must be declared
+            <br /><br />
         </div>
         <a href="battle.php" type="button" class="btn btn-lg btn-primary btn-block">Start Battle</a>
 
